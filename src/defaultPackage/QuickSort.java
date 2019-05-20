a848a0a0156307e874f188035f536f616a15373a
Adam Xu <xxu53@wisc.edu>
2019-05-21 02:28:21 +0800

    two partition algorithms for quicksort

--- src/defaultPackage/QuickSort.java
+++ src/defaultPackage/QuickSort.java
@@ -1,77 +1,75 @@
 package defaultPackage;
-
+/*
+ *	Geeks for Geeks website , quicksort
+ */
 public class QuickSort {
 	//Initial parameters:
 	//low: 0
 	//high: arr.length-1  (position of last element of array)
 	
-	 static void quickSort(int arr[], int low, int high) 
+	 public void quickSort(int arr[], int low, int high) 
 	    { 
 	        if (low < high) 
 	        { 	
-	            //pick p as pivot
-	            int p = partition(arr, low, high); 						
-	            //recursively sort the left and right array of pivot
-	            quickSort(arr, low, p-1); 			
-	            quickSort(arr, p+1, high);			
+	           //bLomuto:
+	           //int p = partition(arr, low, high); 						
+	           // quickSort(arr, low, p-1); 			
+	           // quickSort(arr, p+1, high);			
+	           //Hoare:
+	        	int p = partition(arr,low,high);
+	        	//System.out.println(p);
+	        	quickSort(arr,low,p-1);
+	        	quickSort(arr,p+1,high);
 	        } 
 	        //stops if there's only 1 element
 	    } 
-	 
-	public static int partition(int[] arr, int low, int high) 
+	 //bLomuto partition scheme:
+	private int partition(int[] arr, int low, int high) 
     { 
-        int pivot = arr[(low+high)/2]; 	//pivot = middle element
-        int i= low;						//counter, starts at the left side of array, moves toward right
-        int j= high;					//counter, starts at the right side of array, moves toward left
-        //System.out.println("Partition Called");
-        while(i<=j) {     	
-        	//find the first element that is smaller than pivot (from left to right)
-	        while(arr[i]<pivot) {
-	        	//System.out.println("Program reached this i while loop with i="+i+" with pivot ="+pivot);
-	        	i++;
-	        }
-	        //find the first element that is larger than pivot (from right to left)
-	        while(arr[j]>pivot) {
-	        	//System.out.println("Program reached this j while loop with j="+j+"with pivot ="+pivot);
-	        	j--;
-	        }
-	        //switch them (element bigger than pivot should stay in the right of the pivot. vise versa)
-	        //then move on
-        	 if(i<=j) {
-	        	 swapPosition(arr,i,j);
-	        	 //System.out.println("After Swap:");
-	        	 //printArray(arr);
-	        	 //System.out.println("i="+i+" j="+j+" pivot="+pivot);
-	        	 i++;
-	        	 j--;
-        	 }
-        }
-       //after everything finishes, return the pivot position
-       return j;
+       int pivot = arr[high]; 			//pivot = last element
+       int i= low-1;					
+       
+	   for(int j= low;j<high;j++) {		//swap elements>=pivots to the right side
+		   if(arr[j]<=pivot) {
+			   i++;
+			   swap(arr,i,j);
+		   }
+	   }
+	   swap(arr,i+1,high);
+      
+       return i+1;
     } 
+	//Hoare partition scheme:
+	private int partition2 (int[]arr, int low, int high) {
+		int pivot = arr[low+(high-low)/2];	//pick the middle element
+		int i = low;						//find element >pivot
+		int j = high;						//find element <pivot
+											//swap them.
+		while(true) {
+			while(arr[i]<pivot) {
+				i++;
+			}
+			while(arr[j]>pivot && j>0) {
+				j--;
+			}
+			if(i<j) {
+				swap(arr,i,j);
+				i++;
+				j--;
+			}else {
+				break;
+			}
+		}
+		return i;
+		
+	}
 	
-	//method to swap position of two array elements
-	public static void swapPosition(int[] arr,int a,int b) {
+	
+	//Swap position of two array elements
+	private void swap(int[] arr,int a,int b) {
 				 int temp= arr[b];
 				 arr[b]=arr[a];
 				 arr[a]=temp;
 			 }
-		
-		
-	//Testing it with multiple same elements
-	public static void main(String[] args) {
-		int[]arr= {10,10,10,9,9,8,8,8,1};
-		quickSort(arr,0,arr.length-1);
-		printArray(arr);
-	}
-	
-	//method to print the whole array
-	public static void printArray(int[]arr) {
-		for(int y=0;y<arr.length;y++) {
-			System.out.print(arr[y]+",");
-			
-		}
-		System.out.println("");
-	}
 
 }
